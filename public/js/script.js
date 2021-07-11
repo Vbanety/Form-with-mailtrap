ff1069647b937df4b23127c589b724e8ed095c14
Vinicius Batista <vbanetyy@gmail.com>
2021-07-11 19:40:00 -0300

    Insert modal poupup

--- public/js/script.js
+++ public/js/script.js
@@ -3,17 +3,23 @@ const btn = document.querySelector('button');
 
 const inputs = document.querySelector('form');
 
+const modalEmail = document.getElementById('modal');
+
 btn.addEventListener('click', () => {
+    
     Email.send({
         Host: "smtp.mailtrap.io",
         Username: "bf7872d2c0e993",
         Password: "85ec3affff5fc6",
         To: "batistavesuporte@gmail.com",
         From: inputs.elements["email"].value,
-        Subject: "Recado do portfolio",
+        Subject: "Recado do Mailtrap",
         Body: inputs.elements["subjects"].value 
         + "<br><br>" + inputs.elements["name"].value 
         + "<br><br>" + inputs.elements["message"].value
-    }).then(msg=> alert('Email enviado com sucesso!!'))
+    }).then(modalEmail.classList.add('afterSend'));
 })
 
+function closeMessage() {
+    modalEmail.classList.remove('afterSend');
+}
